aaabc8364b776842af9be7c2e58a4225523ced29
Natallia Samoilik <n.samoilik@sportradar.com>
2024-11-22 16:56:55 +0100

    fourth test is done

--- pages/inventoryPage.js
+++ pages/inventoryPage.js
@@ -2,6 +2,8 @@ class InventoryPage {
    
     constructor(page) {
         this.page = page;
+        this.burgerMenue = page.getByRole('button', { name: 'Open Menu' });
+        this.logout = page.locator('[data-test="logout-sidebar-link"]');
 
         // Selectors
        
@@ -15,6 +17,15 @@ class InventoryPage {
         await this.page.goto('https://www.saucedemo.com/inventory.html');
     }
 
+    // open burger menue
+    async openBurgerMenue() {
+        await this.burgerMenue.click();
+    }
+
+     // click logout from burger menue
+     async logoutClick() {
+        await this.logout.click();
+    }
 }
 
 module.exports = InventoryPage;
---
+++ tests/auth/scenario4.spec.js
@@ -0,0 +1,28 @@
+const { test, expect } = require('@playwright/test');
+const LoginPage = require('../../pages/loginPage');
+const InventoryPage = require('../../pages/inventoryPage');
+
+import {
+    loginPageUrl,
+    standardUser,
+    password,
+} from '../../config'
+
+test('SCENARIO: 4. User should be logged out once Logout button is pressed', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    const inventoryPage = new InventoryPage(page);
+
+    await test.step('GIVEN: registered user is on the inventory page', async () => {
+        await loginPage.openLoginPage();
+        await loginPage.login(standardUser, password);
+    });
+
+    await test.step('WHEN: in burger menue user presses logout button', async () => {
+       await inventoryPage.openBurgerMenue();
+       await inventoryPage.logoutClick();
+    });
+
+    await test.step('THEN: user is redirected to the login page', async () => {
+      await expect(page).toHaveURL(loginPageUrl);
+    });
+});
